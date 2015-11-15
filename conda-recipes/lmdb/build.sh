f9932ae15777c4b336569c34cfbaf0e3670c817d
Victor Escorcia <escorciav@gmail.com>
2015-11-15 18:29:26 +0300

    [conda] Hack to build lmdb properly

--- conda-recipes/lmdb/build.sh
+++ conda-recipes/lmdb/build.sh
@@ -1,6 +1,10 @@
 cd liblmdb
 sed -i 's@prefix\t= /usr/local@prefix\t?= /usr/local@g' Makefile
+sed -i 's@$(DESTDIR)$(prefix)/bin;@$(DESTDIR)$(prefix)/bin/;@g' Makefile
+sed -i 's@$(DESTDIR)$(prefix)/include;@$(DESTDIR)$(prefix)/include/;@g' Makefile
+sed -i 's@$(DESTDIR)$(prefix)/lib;@$(DESTDIR)$(prefix)/lib/;@g' Makefile
+sed -i 's@$(DESTDIR)$(prefix)/man/man1;@$(DESTDIR)$(prefix)/man/man1/;@g' Makefile
 make all
 
-mkdir $PREFIX/man
+for i in bin lib include man/man1; do mkdir -p $PREFIX/$i; done
 make install prefix=$PREFIX
